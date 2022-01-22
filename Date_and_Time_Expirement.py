b6813a46cbd416067dd5b2279c524951db1f2f3b
Verisimilitude <acharyapiyush1@outlook.com>
2022-01-21 16:53:34 -0800

    Add files via upload

---
+++ Date_and_Time_Expirement.py
@@ -0,0 +1,7 @@
+import datetime
+
+e = datetime.datetime.now()
+
+print ("Today's date:  = %s/%s/%s" % (e.month, e.day, e.year))
+
+print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
